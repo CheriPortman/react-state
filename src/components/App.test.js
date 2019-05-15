7cd9927303fea9968f79e0cdc423946e581e330f
Cheri Portman <cheriportman@hotmail.com>
2019-05-14 17:02:11 -0700

    error in app test fixed, still passes (?)

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import App from './App';
-import { exportAllDeclaration } from '@babel/types';
 
 describe('App componenet', () => {
   it('renders App', () => {
